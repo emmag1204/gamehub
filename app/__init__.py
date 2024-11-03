13c63fff420ad1f3d3c29b023ec3e9ebf2339db5
Emma Alfaro <emma.alfarod@gmail.com>
2024-11-02 18:24:04 -0700

    first version of styles

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app():
     from .routes import main
     app.register_blueprint(main)
 
-    @ app.cli.command('seed')
+    @app.cli.command('seed')
     def seed_db():
         from .models import Section, Game
 
@@ -33,9 +33,21 @@ def create_app():
 
         if not Game.query.first():
             games = [
-                Game(section='Competitive', name='League of Legends', description='MOBA game', platform='PC', image='lol.jpg'),
-                Game(section='Multiplayer', name='Among Us', description='Social deduction game', platform='PC, Mobile', image='amongus.jpg'),
-                Game(section='Adventure', name='Zelda', description='Action-adventure', platform='Switch', image='zelda.jpg'),
+               Game(section='Competitive', name='League of Legends', description='A team strategy game where players control unique characters and try to destroy the enemy’s base.', platform='PC', image='lol.jpg'),
+                Game(section='Multiplayer', name='Super Smash Bros.', description='A fighting game where famous Nintendo characters battle in different arenas.', platform='Switch', image='smash.jpg'),
+                Game(section='Multiplayer', name='Mario Kart', description='A racing game with Nintendo characters who use special powers to win.', platform='Switch', image='mariokart.jpg'),
+                Game(section='Multiplayer', name='Mario Party', description='A party game full of mini-games where players compete on boards to earn stars and coins.', platform='Switch', image='marioparty.jpg'),
+                Game(section='Adventure', name='Minecraft', description='A building and adventure game where players explore and create worlds made of blocks.', platform='PC, Mobile, Console', image='minecraft.jpg'),
+                Game(section='Multiplayer', name='Roblox', description='An online platform where users can create and play a variety of games made by the community.', platform='PC, Mobile', image='roblox.jpg'),
+                Game(section='Competitive', name='Valorant', description='A shooting game where teams use unique character abilities to compete.', platform='PC', image='valorant.jpg'),
+                Game(section='Adventure', name='Genshin Impact', description='An action game where players explore a fantasy world with magical characters.', platform='PC, Mobile', image='genshin.jpg'),
+                Game(section='Competitive', name='Clash of Clans', description='A mobile strategy game where players build villages and battle others to gather resources.', platform='Mobile', image='clashofclans.jpg'),
+                Game(section='Competitive', name='Fortnite', description='A game where players compete to be the last one standing on a large island.', platform='PC, Console', image='fortnite.jpg'),
+                Game(section='Competitive', name='Apex Legends', description='A survival game where teams of unique characters compete to be the last standing.', platform='PC, Console', image='apex.jpg'),
+                Game(section='Multiplayer', name='Overcooked 2', description='A cooking game where players work together to prepare meals under time pressure.', platform='PC, Console', image='overcooked2.jpg'),
+                Game(section='Adventure', name='Pokemon', description='An adventure game where players catch and train creatures called Pokémon to battle others.', platform='Switch', image='pokemon.jpg'),
+                Game(section='Adventure', name='Animal Crossing', description='A life simulation game where players develop an island into a community of animal friends.', platform='Switch', image='animalcrossing.jpg'),
+                Game(section='Multiplayer', name='Rocket League', description='A game that combines soccer and cars, where players use rocket-powered cars to score goals.', platform='PC, Console', image='rocketleague.jpg')
             ]
             db.session.bulk_save_objects(games)
             db.session.commit()
--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,29 @@
 import os
-from flask import Blueprint, request, jsonify, url_for
+from flask import Blueprint, request, jsonify, url_for, render_template, current_app
 from werkzeug.utils import secure_filename
 from .models import Game, Section
 from . import db
 
 main = Blueprint('main', __name__)
 
-@main.route('/sections', methods=['GET'])
-def get_sections():
-    sections = Section.query.all()
-    return jsonify([{'name': section.name, 'image': url_for('static', filename=f'images/{section.image}')} for section in sections])
+@main.route('/')
+def home():
+    sections = Section.query.all()  
+    return render_template('index.html', sections=sections)  
 
-@main.route('/games', methods=['GET'])
-def get_games_by_section():
-    section = request.args.get('section')
-    games = Game.query.filter_by(section=section).all()
-    return jsonify([{'name': game.name, 'description': game.description, 'platform': game.platform, 'image': url_for('static', filename=f'images/{game.image}')} for game in games])
-
-@main.route('/game/<int:id>', methods=['GET'])
-def get_game(id):
-    game = Game.query.get_or_404(id)
-    return jsonify({
-        'name': game.name,
-        'description': game.description,
-        'platform': game.platform,
-        'image': url_for('static', filename=f'images/{game.image}')
-    })
+@main.route('/games/<section_name>', methods=['GET'])
+def get_games_by_section(section_name):
+    games = Game.query.filter_by(section=section_name).all() 
+    return render_template('games.html', games=games, section_name=section_name)  
 
 @main.route('/game', methods=['POST'])
 def add_game():
-    data = request.form
-    file = request.files['image']
+    data = request.form  
+    file = request.files['image']  
 
     if file:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(request.app.config['UPLOAD_FOLDER'], filename))
+        filename = secure_filename(file.filename)  
+        file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename)) 
 
     new_game = Game(
         section=data['section'],
@@ -44,7 +33,11 @@ def add_game():
         image=filename
     )
 
-    db.session.add(new_game)
-    db.session.commit()
+    db.session.add(new_game)  
+    db.session.commit()  
 
-    return jsonify({'message': 'Game created successfully!'})
+    return jsonify({'message': 'Game created successfully!'})  
+
+@main.route('/add-game', methods=['GET'])
+def add_game_page():
+    return render_template('add_game.html') 
