f64961da24b97aaca25100b2694011acfbf193b8
Emma Alfaro <emma.alfarod@gmail.com>
2024-12-04 16:02:37 -0800

    add new game working

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, request, jsonify, url_for, render_template, current_app
+from flask import Blueprint, request, jsonify, url_for, render_template, current_app, redirect
 from werkzeug.utils import secure_filename
 from .models import Game, Section
 from . import db
@@ -9,12 +9,12 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     sections = Section.query.all()  
-    return render_template('index.html', sections=sections)  
+    return render_template('index.html', sections=sections)
 
 @main.route('/games/<section_name>', methods=['GET'])
 def get_games_by_section(section_name):
     games = Game.query.filter_by(section=section_name).all() 
-    return render_template('games.html', games=games, section_name=section_name)  
+    return render_template('games.html', games=games, section_name=section_name)
 
 @main.route('/game', methods=['POST'])
 def add_game():
@@ -36,8 +36,9 @@ def add_game():
     db.session.add(new_game)  
     db.session.commit()  
 
-    return jsonify({'message': 'Game created successfully!'})  
+    return redirect(url_for('main.home'))
 
 @main.route('/add-game', methods=['GET'])
 def add_game_page():
-    return render_template('add_game.html') 
+    sections = Section.query.all()  
+    return render_template('add_game.html', sections=sections)
